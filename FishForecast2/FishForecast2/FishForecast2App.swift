2e5ecb4650dc652c3fe83bb8b03d148ea45bf65f
Daniel Smith <daniel@example.com>
2025-08-09 09:08:27 -0500

    AddExcode project with SwiftUI template

---
+++ FishForecast2/FishForecast2/FishForecast2App.swift
@@ -0,0 +1,17 @@
+//
+//  FishForecast2App.swift
+//  FishForecast2
+//
+//  Created by Daniel Streeter on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FishForecast2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
